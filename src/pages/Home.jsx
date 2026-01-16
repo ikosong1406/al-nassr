7bd8f4a58307eaa860f2c24217f37318d845c629
ikosong14 <alexandervirtuous14@gmail.com>
2026-01-17 00:07:29 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -158,7 +158,7 @@ const Home = () => {
         </div>
 
         {/* Main content grid */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 gap-6">
           {/* TradingView Widget - Full width on mobile, 2/3 on desktop */}
           <div className="lg:col-span-2">
             <div className="h-[500px]">
